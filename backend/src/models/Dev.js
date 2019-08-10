23226365811e077788c1dd5bdd39922d1631aeae
Douglas Pinheiro <dougcpinheiro@yahoo.com>
2019-08-10 10:56:59 +0100

    [Aula 1] - Conhecendo a Omnistack

---
+++ backend/src/models/Dev.js
@@ -0,0 +1,29 @@
+const {Schema, model} = require('mongoose');
+
+const DevSchema = Schema({
+    name : {
+        type: String,
+        require: true,
+    },
+    user : {
+        type: String,
+        require: true,
+    },
+    bio : String,
+    avatar: {
+        type: String,
+        required: true,
+    },
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev',
+    }],
+    dislikes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev',
+    }],
+}, {
+    timestamps: true
+});
+
+module.exports = model('Dev', DevSchema);
