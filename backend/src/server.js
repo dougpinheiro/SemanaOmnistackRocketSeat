c6d70b76df2c09a1567bcc77733497b917f768d5
Douglas Pinheiro <dougcpinheiro@yahoo.com>
2019-08-11 13:24:42 +0100

    [Aula 2] - Construindo a interface Web

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
-const authentication = require('./config/dbconfig');
+const auth = require('./config/dbconfig');
 
 const server = express();
 
-mongoose.connect(`mongodb+srv://${authentication.login}:${authentication.login}@cluster0-fakef.mongodb.net/omnistack8?retryWrites=true&w=majority`, {
+const {login, password} = auth;
+
+const url = `mongodb+srv://${login}:${password}@cluster0-fakef.mongodb.net/omnistack8?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
     useNewUrlParser : true
 });
 
+console.log('Connected to the database!');
 
-server.use(cors);
+server.use(cors());
 server.use(express.json());
 server.use(routes);
+
+console.log('Server ready!');
 
 server.listen(3333);
